4c53d555e68f83d8a6f7bc4fecb436ba23cd2f10
prasanthfatu <146060960+prasanthfatu@users.noreply.github.com>
2024-07-25 23:16:53 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
-    'https://nexcare.onrender.com'
+    'https://nexcare.onrender.com',
+    'https://nexcare-api.onrender.com/coverPhoto',
+    'https://nexcare-api.onrender.com/img'
 ];
 
 module.exports = allowedOrigins;
